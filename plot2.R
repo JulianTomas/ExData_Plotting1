872841cc6f3182c260fbbe541859ff9e494994ba
JulianTomas <julianortiz248@gmail.com>
2016-10-15 22:37:53 -0700

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,16 @@
+# Address plot1.R 
+plot1 <- function() {
+  hist(df$Global_active_power, main = paste("Global Active Power"), col="red", xlab="Global Active Power (kilowatts)")
+  dev.copy(png, file="plot1.png", width=480, height=480)
+  dev.off()
+  cat("Plot1.png has been saved in", getwd())
+}
+plot1()
+#Address plot2.R
+plot1 <- function() {
+  plot(df$timestamp,df$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+  dev.copy(png, file="plot2.png", width=480, height=480)
+  dev.off()
+  cat("plot2.png has been saved in", getwd())
+}
+plot1()
